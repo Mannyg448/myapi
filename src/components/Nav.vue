0b5147619d16eae811a95d048ef17bba9e0ab381
Mannyg448 <manny.g448@hotmail.com>
2022-04-03 17:26:41 -0500

    chagnes were made for the advice api

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,9 @@
 <template>
    <div class="bg-white text-red-800">
-        <div class="container mx-auto flex items-center italic justify-between ">
-            <h1 class="tracking-tighter text-3xl font-semibold">
-                Basic<span class="font-normal">Router</span>
+        <div class="container mx-auto flex items-center italic justify-center ">
+            <h1 class=" py-8 tracking-tighter text-3xl font-semibold">
+                My<span class="font-normal">Advice</span>
                 </h1>
-            <nav>
-                <ul class="flex space-x-4">
-                    <router-link to="/">
-                    <li class="py-8 px-4  hover:cursor-pointer hover:bg-gray-500 hover:text-white">
-                        Home
-                        </li></router-link>
-                     <router-link to="/about">   
-                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-500 hover:text-white">About</li>
-                    </router-link>
-                    <router-link to="/hello">
-                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-500 hover:text-white">Hello</li>
-                    </router-link>
-
-                     <router-link to="/us">
-                    <li class="py-8 px-4 hover:cursor-pointer hover:bg-gray-500 hover:text-white">Us</li>
-                    </router-link>
-                    
-                </ul>
-
-            </nav>
         </div>
         </div>
     
